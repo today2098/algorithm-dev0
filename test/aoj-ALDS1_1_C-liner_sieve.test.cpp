1a674bc22ebe55ba9438b62585e418f0deb82bad
Today <76273733+today2098@users.noreply.github.com>
2024-07-15 14:52:06 +0900

    [add] Create a class, "LinerSieve"

---
+++ test/aoj-ALDS1_1_C-liner_sieve.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_1_C"
+
+#include <iostream>
+
+#include "../lib/Math/NumberTheory/liner_sieve.hpp"
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    constexpr int mx = 1e8;
+    algorithm::LinearSieve sieve(mx);
+
+    int ans = 0;
+    for(int i = 0; i < n; ++i) {
+        int a;
+        std::cin >> a;
+
+        if(sieve.is_prime(a)) ans++;
+    }
+
+    std::cout << ans << std::endl;
+}
