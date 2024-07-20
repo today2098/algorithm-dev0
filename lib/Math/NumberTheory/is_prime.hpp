99fdf0c5390b1832622fbf4e87a9e8cff8d7abd7
Today <76273733+today2098@users.noreply.github.com>
2024-07-20 12:50:30 +0900

    [clean] Refactoring

--- lib/Math/NumberTheory/is_prime.hpp
+++ lib/Math/NumberTheory/is_prime.hpp
@@ -11,12 +11,13 @@
 namespace algorithm {
 
 // 素数判定．O(√N).
-bool is_prime(long long n) {
+template <typename Type>
+bool is_prime(Type n) {
     assert(n >= 0);
     if(n < 2) return false;
     if(n == 2) return true;
     if(n % 2 == 0) return false;
-    for(long long p = 3; p * p <= n; p += 2) {
+    for(Type p = 3; p * p <= n; p += 2) {
         if(n % p == 0) return false;
     }
     return true;
